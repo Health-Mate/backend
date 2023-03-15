3f9a2d8889fa3290367da0621bf976bda0127923
KeanuNeo <matrixneodev@gmail.com>
2023-03-16 00:06:57 +0300

    [+] added: simple HTTP server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+using ip::tcp;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  io_service io_service;
+  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 8080));
+
+  while (true) {
+    tcp::socket socket(io_service);
+    acceptor.accept(socket);
+
+    std::string message = "Hello, world!";
+    std::stringstream ss;
+    ss << "HTTP/1.1 200 OK\r\n";
+    ss << "Content-Type: text/plain\r\n";
+    ss << "Content-Length: " << message.length() << "\r\n";
+    ss << "\r\n";
+    ss << message;
+
+    std::string response = ss.str();
+    boost::system::error_code ignored_error;
+    write(socket, buffer(response), ignored_error);
+  }
+
+  return 0;
 }
